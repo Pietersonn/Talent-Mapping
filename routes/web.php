f4746ecf628746517ab57fd19c873118e4fc7460
pietersonn <pietersonnn@gmail.com>
2025-11-03 06:09:30 +0800

    Bismillah

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 // PUBLIC
 use App\Http\Controllers\Public\HomeController;
 use App\Http\Controllers\Public\GuideController;
+use Illuminate\Support\Facades\Mail;
 
 // PROFILE (semua user login)
 use App\Http\Controllers\ProfileController;
@@ -14,6 +15,13 @@ use App\Http\Controllers\ProfileController;
 // --------------------
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/panduan-hasil', [GuideController::class, 'index'])->name('guide');
+
+Route::get('/test-email', function () {
+    Mail::raw('Tes kirim lewat SendGrid API', function ($m) {
+        $m->to('alamatmu@gmail.com')->subject('Test Kirim SendGrid API');
+    });
+    return '✅ Email test sudah dikirim (cek inbox atau spam folder)';
+});
 
 // Auth scaffolding (login, register, dll)
 require __DIR__.'/auth.php';
