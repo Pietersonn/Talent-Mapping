c1ea7317d0dc0c1c6fc1927519af92165ba3b1a1
pietersonn <pietersonnn@gmail.com>
2025-11-04 15:45:15 +0800

    Report User Fix

--- resources/views/public/pdf/report.blade.php
+++ resources/views/public/pdf/report.blade.php
@@ -65,8 +65,8 @@
   .p5comp { color:#fff; font-weight:700; font-size:16px; text-align:center; }
   .p5desc { color:#fff; font-size:16px; line-height:1.35; width:150mm; left:125mm; font-weight:700 }
   .p5comp2{ top:81mm; left:45mm;  width:65mm; }
-  .p5comp1{ top:72mm; left:123mm; width:65mm; }
-  .p5comp3{ top:83mm; left:186mm; width:65mm; }
+  .p5comp1{ top:72mm; left:50%; width:65mm; }
+  .p5comp3{ top:83mm; left:185mm; width:65mm; }
   .p5desc1{ top:129mm; left:127mm; }
   .p5desc2{ top:152mm; left:127mm; }
   .p5desc3{ top:176mm; left:127mm; }
